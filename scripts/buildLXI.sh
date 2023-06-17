01edaf3daeedd7e17bc02e41545dd35e2edd7cc2
Joe Mueller <joseph.e.mueller@gmail.com>
2023-06-16 22:05:39 -0600

    INitial work - just checkpoint

---
+++ scripts/buildLXI.sh
@@ -0,0 +1,7 @@
+#!bash
+# Script to kick off various Python programs to build various portions
+# of the LXI public website (public.lxistandard.org)
+
+# this is run from the root of the LxiStandard.github.io repo
+
+python scripts/buildSpecificationPages.py
---
+++ site/specifications.md
@@ -0,0 +1,7 @@
+---
+layout: default
+title: Specification Downloads
+nav_order:  2
+---
+
+This child pages of this page have links to the LXI specifications.
